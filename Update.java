a1f4fd0281e480e99b402753bf560abadeaaf314
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-10-29 10:46:34 +0530

    User Edit file added

--- Update.java
+++ Update.java
@@ -1,44 +1,47 @@
 import java.util.*;
 public class Update {
+	//to update user details//
 	Map<String, String> ShowString = new HashMap<String, String>();
 	HashMap<String, Integer> ShowInt = new HashMap<>();
+	//option from users taken//
 	System.out.println("Select option to replace\n 1.First name\n 2.Lastname\n"
 			+ "3.Address\n 4.city\n 5.state\n 6.Zip\n 7.PhoneNo");
 	int UpdateOption = update.nextInt();
-	
+	//user cooses the option to update// 
 	switch(UpdateOption) {
 			case 1:System.out.println("enter firstname:");
-				   String firstName1 = update.next();
-				   ShowString.replace( "FirstName", firstName1);
-				   break;
+			       String firstName1 = update.next();
+			       ShowString.replace( "FirstName", firstName1);
+			       break;
 			case 2:System.out.println("enter Last Name:");
 			       String lastName1 = update.next();
 			       ShowString.replace( "LastName", lastName1);
 			       break;
 			case 3:System.out.println("enter Last Name:");
-		           String address1 = update.next();
-		           ShowString.replace( "Address", address1);
-		           break;
+		               String address1 = update.next();
+		               ShowString.replace( "Address", address1);
+		               break;
 			case 4:System.out.println("enter Last Name:");
-	               String city1 = update.next();
-	               ShowString.replace( "City", city1);
-	               break;
+	                       String city1 = update.next();
+	                       ShowString.replace( "City", city1);
+	                       break;
 			case 5:System.out.println("enter Last Name:");
-	               String State1 = update.next();
-	               ShowString.replace( "State", State1);
-	               break;
+	                       String State1 = update.next();
+	                       ShowString.replace( "State", State1);
+	                       break;
 			case 6:System.out.println("enter Last Name:");
-	               int zip1 = update.nextInt();
-	               ShowInt.replace( "Zip", zip1);
-	               break;
+	                       int zip1 = update.nextInt();
+	                       ShowInt.replace( "Zip", zip1);
+	                       break;
 			case 7:System.out.println("enter Last Name:");
-	               int PhoneNo1 = update.nextInt();
-	               ShowInt.replace( "PhoneNumber", PhoneNo1);
-	               break;
+	                       int PhoneNo1 = update.nextInt();
+	                       ShowInt.replace( "PhoneNumber", PhoneNo1);
+	                       break;
 	         
 	         default:
 	        	 	 System.out.println("wrong input");
 	}
+	//scanner is closed//
 	update.close();
 	
 	
